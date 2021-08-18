67e72cd0d8493404dcfd16cb7aeede3b0580e960
Peter Bacenet <peterbacenet@icloud.com>
2021-08-17 20:24:29 -0400

    commit tree 2d3a61e0de65c080e238a23e36b3ef75838c1524

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let result
+  if (feet <= 400) {result = 'This one is on me!'}
+  else if (feet > 2500) {result = 'No can do.'}
+  else if (feet > 2000) {result = 'I will gladly take your thirty bucks.'};
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(city){
+  // let result
+  // if (city === 'NYC') {result = 'Ok, sounds good.'}
+  // else if (city != 'NYC') {result = 'No go.'}
+  // return result
+  let result = city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+  return result
+} 
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(typeoftip){
+let response
+switch(typeoftip){
+  case "generous": response = 'Thank you so much.';
+  break;
+  case "not as generous" : response ="Thank you.";
+  break;
+  default: response = 'Bye.';
+}
+return response;
 }
